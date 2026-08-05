0594f4cb72af991a6c5d0081688e6a73a0d29a19
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-08-05 08:41:47 +0600

    Create clock.c

---
+++ hal/clock.c
@@ -0,0 +1,17 @@
+/**
+ * @file clock.c
+ * @brief OrbitMesh clock HAL.
+ *
+ * Provides platform clock abstraction.
+ *
+ * @copyright Apache License 2.0
+ */
+
+#include "orbitmesh/error.h"
+
+
+om_error_t
+om_clock_init(void)
+{
+    return OM_SUCCESS;
+}
