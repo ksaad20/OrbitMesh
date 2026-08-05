89746a289b8a5aaf18a524e79d825cf21f51bc8b
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-08-06 01:01:57 +0600

    Refactor I2C driver interface and add documentation
    Updated I2C driver interface by removing unnecessary parameters and adding documentation.

--- include/orbitmesh/drivers/i2c_driver.h
+++ include/orbitmesh/drivers/i2c_driver.h
@@ -1,37 +1,65 @@
-#ifndef ORBITMESH_DRIVERS_I2C_DRIVER_H
-#define ORBITMESH_DRIVERS_I2C_DRIVER_H
+/**
+ * @file i2c_driver.h
+ * @brief OrbitMesh I2C driver interface.
+ */
+
+#ifndef ORBITMESH_I2C_DRIVER_H
+#define ORBITMESH_I2C_DRIVER_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "orbitmesh/error.h"
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-#include <stddef.h>
-#include <stdint.h>
 
-#include "orbitmesh/error.h"
-#include "orbitmesh/i2c.h"
-
+/**
+ * @brief Initialize I2C driver.
+ *
+ * @return OrbitMesh error code.
+ */
 om_error_t
-om_i2c_driver_init(
-    om_i2c_id_t i2c);
+om_i2c_driver_init(void);
 
+
+/**
+ * @brief Write data to an I2C device.
+ *
+ * @param address Device address.
+ * @param data Data buffer.
+ * @param length Number of bytes.
+ *
+ * @return OrbitMesh error code.
+ */
 om_error_t
 om_i2c_driver_write(
-    om_i2c_id_t i2c,
     uint8_t address,
     const uint8_t *data,
     size_t length);
 
+
+/**
+ * @brief Read data from an I2C device.
+ *
+ * @param address Device address.
+ * @param data Receive buffer.
+ * @param length Number of bytes.
+ *
+ * @return OrbitMesh error code.
+ */
 om_error_t
 om_i2c_driver_read(
-    om_i2c_id_t i2c,
     uint8_t address,
     uint8_t *data,
     size_t length);
+
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif /* ORBITMESH_I2C_DRIVER_H */
