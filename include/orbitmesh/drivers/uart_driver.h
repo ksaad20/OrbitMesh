40fd4c5f4de600e3adf2beb6806e8da67b32f751
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-08-05 23:48:09 +0600

    Refactor UART driver interface to simplify function signatures
    Removed UART peripheral identifier parameter from initialization and read/write functions.

--- include/orbitmesh/drivers/uart_driver.h
+++ include/orbitmesh/drivers/uart_driver.h
@@ -1,10 +1,3 @@
-/**
- * @file uart.h
- * @brief OrbitMesh UART driver interface.
- *
- * @copyright Apache License 2.0
- */
-
 #ifndef ORBITMESH_DRIVERS_UART_H
 #define ORBITMESH_DRIVERS_UART_H
 
@@ -17,49 +10,20 @@ extern "C"
 #include <stdint.h>
 
 #include "orbitmesh/error.h"
-#include "orbitmesh/uart.h"
 
 
-/**
- * @brief Initialize UART driver.
- *
- * @param uart UART peripheral identifier.
- *
- * @return OrbitMesh error code.
- */
 om_error_t
-om_uart_driver_init(
-    om_uart_id_t uart);
+om_uart_driver_init(void);
 
 
-/**
- * @brief Send UART data.
- *
- * @param uart UART peripheral identifier.
- * @param data Data buffer.
- * @param length Data length.
- *
- * @return OrbitMesh error code.
- */
 om_error_t
 om_uart_driver_write(
-    om_uart_id_t uart,
     const uint8_t *data,
     size_t length);
 
 
-/**
- * @brief Receive UART data.
- *
- * @param uart UART peripheral identifier.
- * @param data Receive buffer.
- * @param length Buffer length.
- *
- * @return OrbitMesh error code.
- */
 om_error_t
 om_uart_driver_read(
-    om_uart_id_t uart,
     uint8_t *data,
     size_t length);
 
