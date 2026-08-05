44e4b73eb9a59b2f10dc290243d3911089965e3d
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-08-06 02:01:08 +0600

    Refactor LED driver to use global LED pin variable

--- drivers/led_driver.c
+++ drivers/led_driver.c
@@ -1,52 +1,49 @@
+/**
+ * @file led_driver.c
+ * @brief OrbitMesh LED driver.
+ */
+
 #include "orbitmesh/drivers/led_driver.h"
 
 #include "orbitmesh/gpio.h"
 
-static om_pin_t led_pin;
+static om_pin_t g_led_pin = 0U;
 
 om_error_t
-om_led_init(om_pin_t pin)
+om_led_init(
+    om_pin_t pin)
 {
-    led_pin = pin;
+    g_led_pin = pin;
 
     const om_gpio_config_t config = {
+        .pin = pin,
         .mode = OM_GPIO_OUTPUT,
-        .pull = OM_GPIO_NO_PULL,
+        .pull = OM_GPIO_PULL_NONE,
     };
 
     return om_gpio_configure(
-        led_pin,
-        &config
-    );
+        &config);
 }
 
 om_error_t
 om_led_on(void)
 {
     return om_gpio_write(
-        led_pin,
-        true
-    );
+        g_led_pin,
+        OM_GPIO_HIGH);
 }
 
 om_error_t
 om_led_off(void)
 {
     return om_gpio_write(
-        led_pin,
-        false
-    );
+        g_led_pin,
+        OM_GPIO_LOW);
 }
 
 om_error_t
 om_led_toggle(void)
 {
-    static bool state = false;
-
-    state = !state;
-
-    return om_gpio_write(
-        led_pin,
-        state
-    );
+    return om_gpio_toggle(
+        g_led_pin);
 }
