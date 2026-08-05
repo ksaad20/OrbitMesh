3f1a5381a774ce03acee867c08d906ebce728fb9
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-08-05 21:21:10 +0600

    Reorganize includes and structure in task_internal.h

--- kernel/task_internal.h
+++ kernel/task_internal.h
@@ -6,55 +6,44 @@
 #ifndef ORBITMESH_TASK_INTERNAL_H
 #define ORBITMESH_TASK_INTERNAL_H
 
-#include "kernel_internal.h"
-#include "task_control_block.h"
-
-extern om_task_t g_tasks[OM_CONFIG_MAX_TASKS];
-extern bool g_task_used[OM_CONFIG_MAX_TASKS];
-extern om_task_t *g_current_task;
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
+#include <stdbool.h>
+
+#include "kernel_internal.h"
+#include "task_control_block.h"
+
+/*==============================================================================
+ * Constants
+ *============================================================================*/
+
 #define OM_TASK_INVALID_ID ((om_task_id_t)0U)
 
-struct om_task
-{
-    om_task_id_t id;
+/*==============================================================================
+ * Global Task Storage
+ *============================================================================*/
 
-    const char *name;
+extern om_task_t g_tasks[OM_CONFIG_MAX_TASKS];
 
-    om_task_entry_t entry;
+extern bool g_task_used[OM_CONFIG_MAX_TASKS];
 
-    void *argument;
+extern om_task_t *g_current_task;
 
-    void *stack;
-
-    om_size_t stack_size;
-
-    om_priority_t priority;
-
-    om_task_state_t state;
-
-    om_tick_t delay_ticks;
-
-    struct om_task *next;
-};
-
+/*==============================================================================
+ * Internal API
+ *============================================================================*/
 
 om_error_t
 om_task_init(void);
 
-
 om_task_t *
 om_task_idle(void);
 
-
 void
 om_task_tick(void);
-
 
 #ifdef __cplusplus
 }
